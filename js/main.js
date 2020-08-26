b4f9ca9f5b9dddccaa51bcf26f4165f30d535d0f
mkpi-1717 <marekpisarczyk22@gmail.com>
2020-08-26 20:46:44 +0200

    zadanie29

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 4;
+let secondNumber = 3;
+
+function multiplay(firstNumber, secondNumber){
+    let result = firstNumber * secondNumber;
+
+    return result;
+}
+let result = multiplay(firstNumber, secondNumber);
+console.log(result);
